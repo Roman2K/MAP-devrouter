31be108d7d8c9a96a0be442037bb1db2690fe84b
Roman Le Négrate <roman.lenegrate@gmail.com>
2015-04-16 21:31:46 +0200

    proxy /storage/* to MAP-storage

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"path/filepath"
 	"regexp"
 	"runtime"
+	"strings"
 
 	log "gopkg.in/Sirupsen/logrus.v0"
 
@@ -17,10 +18,12 @@ import (
 
 // config
 var addr = flag.String("addr", ":8080", "Address to listen on")
-var mapURL = flag.String("map", "http://localhost:3000", "MAP Rails server address")
+var mapURL = flag.String("map", "http://localhost:3000", "MAP (ecommerce) server URL")
+var storageURL = flag.String("storage", "http://localhost:3010", "MAP-storage server URL")
 
 // package globals
 var mapProxy *httputil.ReverseProxy
+var storageProxy *httputil.ReverseProxy
 var static = map[string]http.HandlerFunc{}
 var public = http.Dir("../map/public")
 var publicHandler http.Handler
@@ -46,27 +49,33 @@ func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	flag.Parse()
-	url, err := url.Parse(*mapURL)
+	setProxy(&mapProxy, *mapURL)
+	setProxy(&storageProxy, *storageURL)
+
+	log.SetLevel(log.DebugLevel)
+	log.Infof("proxying MAP requests to %s", *mapURL)
+	log.Infof("proxying MAP-storage requests to %s", *storageURL)
+	log.Infof("listening on %s", *addr)
+	log.Fatal(http.ListenAndServe(*addr, http.HandlerFunc(route)))
+}
+
+func setProxy(ptr **httputil.ReverseProxy, rawurl string) {
+	url, err := url.Parse(rawurl)
 	if err != nil {
 		log.Fatal(err)
 	}
-	mapProxy = httputil.NewSingleHostReverseProxy(url)
-
-	log.SetLevel(log.DebugLevel)
-	log.Infof("Proxying MAP requests to %s", url)
-	log.Infof("Listening on %s", *addr)
-	log.Fatal(http.ListenAndServe(*addr, http.HandlerFunc(route)))
+	*ptr = httputil.NewSingleHostReverseProxy(url)
 }
 
 func route(w http.ResponseWriter, r *http.Request) {
 	rlog := log.WithFields(log.Fields{"path": r.URL.Path})
 	if h := static[r.URL.Path]; h != nil {
-		rlog.Infof("static path match")
+		rlog.Info("static path match")
 		h(w, r)
 		return
 	}
 	if m := uploadsCMSRe.FindStringSubmatch(r.URL.Path); m != nil {
-		rlog.Infof("sending uploads_cms")
+		rlog.Info("sending uploads_cms")
 		r.URL.Path = fmt.Sprintf("/uploads/cms/%s/image/%s/%s/%s", m[1], m[2], m[3], m[4])
 		publicHandler.ServeHTTP(w, r)
 		return
@@ -78,10 +87,17 @@ func route(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	if ok {
-		rlog.Infof("sending file")
+		rlog.Info("sending file")
 		publicHandler.ServeHTTP(w, r)
 		return
 	}
-	rlog.Infof("proxying to %s", *mapURL)
+	if p := strings.TrimPrefix(r.URL.Path, "/storage/"); len(p) < len(r.URL.Path) {
+		rlog.Info("proxying to MAP-storage")
+		r.URL.Path = "/" + p
+		rlog.Debugf("path rewritten to %s", r.URL.Path)
+		storageProxy.ServeHTTP(w, r)
+		return
+	}
+	rlog.Info("proxying to MAP")
 	mapProxy.ServeHTTP(w, r)
 }
